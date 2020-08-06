d7d1d5c36097076c040fa360ba4b9c05ecece0c7
family gefen <jpgefen@gmail.com>
2020-08-07 00:34:33 +0100

    second commit

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,7 +6,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student s1 = new Student("joe","bloggs",123);
-        s1.toString();
+
+        Student s1 = new Student("Miranda", "Frost", 123001);
+        Student s2 = new Student("Mister", "Bullion", 123002);
+        Student s3 = new Student("Natalya", "Simonova", 123003);
+        Student s4 = new Student("Boris", "Grishenko", 123004);
+
+        StudentList studentList = new StudentList(3);
+        studentList.addStudent(s1);
+        studentList.addStudent(s2);
+        studentList.addStudent(s3);
+
+        studentList.printInfo();
+
+
+
     }
 }
---
+++ src/Main/StudentList.java
@@ -0,0 +1,44 @@
+package Main;
+
+import Student.Student;
+
+public class StudentList {
+
+    private Student[] students;
+    private int maxNumberIntArray;
+    private int currentNumberOfStudentsInArray;
+
+    public StudentList(int maxNumberIntArray) {
+        this.students = new Student[maxNumberIntArray];
+        this.maxNumberIntArray = maxNumberIntArray;
+        this.currentNumberOfStudentsInArray = currentNumberOfStudentsInArray;
+    }
+
+    //checks to see if student already exists in list.
+    public boolean contains(Student student) {
+        for (int i = 0; i < this.students.length; i++) {
+            if (student.equals(i)) {
+                return true;
+            }
+        }return false;
+    }
+
+    //adds student to the list if there is enough space. returns true if added; otherwise, returns false.
+    public boolean addStudent(Student student){
+        if (currentNumberOfStudentsInArray < maxNumberIntArray){
+            students[currentNumberOfStudentsInArray+1] = student;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    public void printInfo() {
+        for (int i =0; i < students.length; i++){
+            if (students[i]!= null){
+                System.out.println(students[i].toString());
+            }
+        }
+    }
+}
