ac1c697f0820f5edefed3038942586ec1dc3acf7
family gefen <jpgefen@gmail.com>
2020-08-14 00:45:02 +0100

    latest commit

--- src/Course/Course.java
+++ src/Course/Course.java
@@ -1,6 +1,30 @@
 package Course;
 
+import Main.StudentList;
+import Student.Student;
+
 public class Course {
 
-    
+    private final int MAX_SIZE_WAIT_LIST = 100;
+    private String title;
+    private String shortTitle;
+    private int MaxClassSize;
+    private StudentList classList;
+    private StudentList waitList;
+
+    public Course(String title, String shortTitle, int maxClassSize) {
+        this.title = title;
+        this.shortTitle = shortTitle;
+        MaxClassSize = maxClassSize;
+        this.classList = new StudentList(maxClassSize);
+        this.waitList = new StudentList(MAX_SIZE_WAIT_LIST);
+    }
+
+    //registers student and assigns him to the relevant list
+    public void register(Student student){
+        if (classList.contains(student)){
+            System.out.println(student + " already is regeistered to " + shortTitle);
+        }
+
+    }
 }
